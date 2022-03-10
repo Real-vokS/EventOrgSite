5a0ee024a61a604e00178f6e13d51e8a879e98e6
Real-vokS <danielcskov@gmail.com>
2022-03-10 09:54:41 +0100

    Event Page

---
+++ EventOrgSite/EventPage.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/UserMasterPage.Master" AutoEventWireup="true" CodeBehind="EventPage.aspx.cs" Inherits="EventOrgSite.EventPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    
+    <link rel="stylesheet" type="text/css" href="/CSS/EventPage.css">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    
+
+</asp:Content>
+
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+
+    <div class="row">
+
+        <div class="col" id="Left-Side" style=" margin-left: 30px;">
+            
+            <h3 class="card-title">PlaceHolder</h3>
+            <div class="card" style="width: 40rem;">
+                <div class="card-body" id="EventDescription">
+                    <p class="card-text">Some placeholder shit that can do other shit</p>
+                </div>
+            </div>
+
+            <div style="margin-top: 40px; width: 40rem">
+                <h5>Date created</h5>
+                <asp:TextBox class="form-control" ID="EventStartDate" Type="date" placeholder="Start Date" runat="server" readonly="true"></asp:TextBox>
+            </div>
+            <div style="margin-top: 20px; width: 40rem">
+                <h5>Sign-up deadline</h5>
+                <asp:TextBox class="form-control" ID="EventDeadline" Type="date" placeholder="Deadline" runat="server" readonly="true"></asp:TextBox>
+            </div>
+        </div>
+
+
+
+        <div class="col" id="RightSide" runat="server" style="margin-top: 144px; padding: 0px 80px 40px 0px">
+
+            <!-- Event Options bliver lavet i Create Event -->
+
+
+
+
+        </div>
+        
+
+    </div>
+
+    
+        <script src="./JS/EventOptions.js"></script>
+
+</asp:Content>
---
+++ EventOrgSite/EventPage.aspx.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EventOrgSite
+{
+    public partial class EventPage : System.Web.UI.Page
+    {
+
+        static List<OrginizedEvent> events = new List<OrginizedEvent>();
+
+        public static void EventList()
+        {
+            events.Clear();
+
+            //foreach loop senere
+            //når data basen er connected
+            for (int i = 0; i < 100; i++)
+            {
+                OrginizedEvent orgEvent = new OrginizedEvent();
+
+                orgEvent.EventId = i;
+                orgEvent.EventTitle = (i + 1).ToString();
+                orgEvent.Participants = 20 + i;
+                orgEvent.CreatedDate = DateTime.Now;
+                orgEvent.Deadline = orgEvent.CreatedDate.AddDays(7);
+
+                events.Add(orgEvent);
+            }
+        }
+
+
+        static string getDivElements()
+        {
+
+            StringWriter sw = new StringWriter();
+
+            using (HtmlTextWriter writer = new HtmlTextWriter(sw))
+            {
+                string classValueDiv1 = "container";
+                string classValueDiv2 = "row clearfix";
+                string classValueDiv3 = "col-md-12 column";
+
+                string classValueTable1 = "table table-bordered table-hover";
+                string idValueTable1 = "tab_logic";
+
+                string classValueTh1 = "text-center";
+
+                string idValueTr1 = "addr0";
+                string idValueTr2 = "addr1";
+
+                string idValueA1 = "add_row";
+                string idValueA2 = "delete_row";
+                string classValueA1 = "btn btn-default pull-left";
+                string classValueA2 = "pull-right btn btn-default";
+
+                string classValueInput1 = "form-control";
+
+
+                //start of 1st div
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, classValueDiv1);
+                writer.RenderBeginTag(HtmlTextWriterTag.Div);
+
+                //start of 2nd div
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, classValueDiv2);
+                writer.RenderBeginTag(HtmlTextWriterTag.Div);
+
+                //start of 3rd div
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, classValueDiv3);
+                writer.RenderBeginTag(HtmlTextWriterTag.Div);
+
+                //start of table
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, classValueTable1);
+                writer.AddAttribute(HtmlTextWriterAttribute.Id, idValueTable1);
+                writer.RenderBeginTag(HtmlTextWriterTag.Table);
+
+                //start of thead
+                writer.RenderBeginTag(HtmlTextWriterTag.Thead);
+
+                //start of tr
+                writer.RenderBeginTag(HtmlTextWriterTag.Tr);
+
+                //start of th #
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, classValueTh1);
+                writer.RenderBeginTag(HtmlTextWriterTag.Th);
+                writer.Write("#");
+                writer.RenderEndTag(); //end of th #
+
+                //start of th Option Name
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, classValueTh1);
+                writer.RenderBeginTag(HtmlTextWriterTag.Th);
+                writer.Write("Option Name");
+                writer.RenderEndTag(); //end of th Option Name
+
+                //start of th Amount
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, classValueTh1);
+                writer.RenderBeginTag(HtmlTextWriterTag.Th);
+                writer.Write("Amount");
+                writer.RenderEndTag(); //end of th Amount
+
+                writer.RenderEndTag(); //end of tr
+                writer.RenderEndTag(); //end of thead
+
+                //start of Tbody
+                writer.RenderBeginTag(HtmlTextWriterTag.Tbody);
+
+                //start of tr addr0
+                writer.AddAttribute(HtmlTextWriterAttribute.Id, idValueTr1);
+                writer.RenderBeginTag(HtmlTextWriterTag.Tr);
+
+                //start of td #
+                writer.RenderBeginTag(HtmlTextWriterTag.Td);
+                writer.Write("1");
+                writer.RenderEndTag(); //end of td #
+
+                //start of td Option Name
+                writer.RenderBeginTag(HtmlTextWriterTag.Td);
+                //start of input
+                writer.AddAttribute(HtmlTextWriterAttribute.Type, "text");
+                writer.AddAttribute(HtmlTextWriterAttribute.Name, "name0");
+                writer.AddAttribute("placeholder", "Name");
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, classValueInput1);
+                writer.RenderBeginTag(HtmlTextWriterTag.Input);
+                writer.RenderEndTag(); //end of input
+                writer.RenderEndTag(); //end of td Option Name
+
+                //start of td Option Name
+                writer.RenderBeginTag(HtmlTextWriterTag.Td);
+                //start of input
+                writer.AddAttribute(HtmlTextWriterAttribute.Type, "text");
+                writer.AddAttribute(HtmlTextWriterAttribute.Name, "amount0");
+                writer.AddAttribute("placeholder", "Amount");
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, classValueInput1);
+                writer.RenderBeginTag(HtmlTextWriterTag.Input);
+                writer.RenderEndTag(); //end of input
+                writer.RenderEndTag(); //end of td Option Name
+
+                writer.RenderEndTag(); // end of tr addr0
+
+                //start of tr addr1
+                writer.AddAttribute(HtmlTextWriterAttribute.Id, idValueTr2);
+                writer.RenderBeginTag(HtmlTextWriterTag.Tr);
+                writer.RenderEndTag(); //end of tr addr 1
+
+                writer.RenderEndTag(); //end of Tbody
+                writer.RenderEndTag(); //end of table
+                writer.RenderEndTag(); //end of 3rd div
+                writer.RenderEndTag(); //end of 2nd div
+
+                //start of a add_row
+                writer.AddAttribute(HtmlTextWriterAttribute.Id, idValueA1);
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, classValueA1);
+                writer.RenderBeginTag(HtmlTextWriterTag.A);
+                writer.Write("Add Row");
+                writer.RenderEndTag(); //end of a add_row
+
+                //start of a delete_row
+                writer.AddAttribute(HtmlTextWriterAttribute.Id, idValueA2);
+                writer.AddAttribute(HtmlTextWriterAttribute.Class, classValueA2);
+                writer.RenderBeginTag(HtmlTextWriterTag.A);
+                writer.Write("Delete Row");
+                writer.RenderEndTag(); //end of a delete_row
+
+                writer.RenderEndTag(); //end of 1st div
+            }
+            return sw.ToString();
+        }
+
+            protected void Page_Load(object sender, EventArgs e)
+            {
+
+                EventList();
+
+
+                if (events.FirstOrDefault().EventId < events.LastOrDefault().EventId)
+                {
+                    events.Reverse();
+                }
+
+                RightSide.InnerHtml = getDivElements();
+
+            }
+        }
+    }
